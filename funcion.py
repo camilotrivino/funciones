ea401bbf7d182dd6ff0327fb94efee2977cc4098
camilotrivino <camilo.trivino@duocuc.cl>
2022-06-14 14:46:25 -0400

    pruebafunciones

---
+++ funcion.py
@@ -0,0 +1,11 @@
+# sin argumentos(entrada) y sin retorno(salida)
+def saludar():
+    saludo="bienvenidos a funciones"
+    print(saludo)
+
+#sin argumentos(entrada) y con retorno(salida)
+def suma():
+    valor1=int(input("ingrese valor 1: "))
+    valor2=int(input("ingrese valor 2: "))
+    total=valor1+valor2
+    return total
--- pruebafuncion.py
+++ pruebafuncion.py
@@ -1,7 +1,12 @@
-def descuento():
+'''def descuento():
     valor=int(input("Ingrese el precio a pagar: "))
     desc=float(input("Ingrese el descuento: "))
     total=valor-valor*(desc/100)
     print(f"TOTAL A PAGAR: {total}")
     return total
-descuento()
+descuento()'''
+import funcion 
+funcion.saludar()
+sumar=funcion.suma()
+print(sumar)
+
