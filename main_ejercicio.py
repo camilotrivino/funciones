8c9fc0e913e877ba9148fea413b234535b2a46ba
camilotrivino <camilo.trivino@duocuc.cl>
2022-06-16 12:30:21 -0400

    terminado(?

--- main_ejercicio.py
+++ main_ejercicio.py
@@ -6,16 +6,20 @@ while menu:
     print("2.- Calcular el descuento de un producto")
     print("3.- Calcular el imc")
     print("4.- Salir")
-    opc=int(input("Ingrese una opcion: "))
-    if opc==1:
-        iva=calculo.iva()
-        print(iva)
-    elif opc==2:
-        descuento=calculo.descuento()
-        print()
-    elif opc==3:
-        imc=calculo.imc()
-        print(imc)
-    elif opc==4:
-        menu=False
-        print("HAS SALIDO")
+    try:
+        opc=int(input("Ingrese una opcion: "))
+        if opc==1:
+            iva=calculo.iva()
+            print(iva)
+        elif opc==2:
+            descuento=calculo.descuento()
+            print()
+        elif opc==3:
+            imc=calculo.imc()
+            print(imc)
+        elif opc==4:
+            menu=False
+            print("HAS SALIDO")
+    except:
+        print("ERROR")
+        
